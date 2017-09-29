dfc95e3ca0ad9c71804f597eb928d6b824a32062
Peter Saliba <sali0075@algonquinlive.com>
2017-09-29 11:20:03 -0400

    Add watch task to Gulp Sass compiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 const gulp = requiere('gulp')
 const gulp = requiere('gulp-sass')
+const sassPath = 'scss/**/*.scss'
 
 gulp.task('sass', funtion () {
   return gulp
     .src('scss/**/*.scss')
-      .pipe(sass())
-      .pipe(gulp.dest('css'))
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+})
+
+gulp.task('default', function () {
+  gulp.watch('sassPath', ['sass'])
 })
